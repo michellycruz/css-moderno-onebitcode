0ed0c8376a4887903de61072559af74f860f0512
Mich <125368412+michellycruz@users.noreply.github.com>
2023-08-24 09:28:11 -0300

    testes com js

---
+++ aulas_sass/08_imports/js/index.js
@@ -0,0 +1,9 @@
+function changeColorOnclick(newColor){
+    const botao = document.getElementById("btn")
+    botao.style.backgroundColor = newColor
+}
+
+function changeColorOnchange(newColor){
+    const botao = document.getElementById("btn")
+    botao.style.backgroundColor = newColor
+}
